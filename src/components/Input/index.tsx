b9150db1370888f815636efbef1bd096706a0b93
Suéllen Castro <castrosuellenx@gmail.com>
2022-09-30 18:34:38 -0300

    add initial components

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Input: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Title>Input</S.Title>
+    </S.Container>
+  );
+};
+
+export default Input;
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Logo: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Title>Logo</S.Title>
+    </S.Container>
+  );
+};
+
+export default Logo;
