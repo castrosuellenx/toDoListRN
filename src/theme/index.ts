6b28f7963f3d11304dc115196ddfba25ee08a65a
Suéllen Castro <castrosuellenx@gmail.com>
2022-09-30 18:35:44 -0300

    add theme

---
+++ src/theme/index.ts
@@ -0,0 +1,26 @@
+export default {
+  colors: {
+    product: {
+      purple: '#8284FA',
+      purpleDark: '#5E60CE',
+      blue: '#4EA8DE',
+      blueDark: '#1E6F9F',
+    },
+
+    base: {
+      gray700: '#0D0D0D',
+      gray600: '#1A1A1A',
+      gray500: '#262626',
+      gray400: '#333333',
+      gray300: '#808080',
+      gray200: '#D9D9D9',
+      gray100: '#F2F2F2',
+      danger: '#E25858',
+    },
+  },
+
+  fonts: {
+    regular: 'Inter_400Regular',
+    bold: 'Inter_700Bold',
+  },
+};
