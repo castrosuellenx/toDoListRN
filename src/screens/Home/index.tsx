00859ccb6f7b8b2ffbc4d5b23d5eb82ed9db8673
Suéllen Castro <castrosuellenx@gmail.com>
2022-10-08 20:09:50 -0300

    added Header on Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
+import {useTheme} from 'styled-components/native';
 
+import Header from '../../components/Header';
 import * as S from './styles';
 
 const Home: React.FC = () => {
+  const theme = useTheme();
+
   return (
     <S.Container>
-      <S.Title>Home</S.Title>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor={theme.colors.base.gray700}
+      />
+
+      <Header />
     </S.Container>
   );
 };
