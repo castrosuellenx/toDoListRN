fb8ff0ba35885ce271e54a14188067ca03cf700d
Suéllen Castro <castrosuellenx@gmail.com>
2022-10-08 22:12:22 -0300

    Input component finished

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {TextInputProps} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import {RFValue} from 'react-native-responsive-fontsize';
+import {useTheme} from 'styled-components/native';
 
 import * as S from './styles';
 
-const Input: React.FC = () => {
+type Props = {
+  handlePressAdd: () => void;
+} & TextInputProps;
+
+const Input: React.FC<Props> = ({handlePressAdd, ...res}) => {
+  const theme = useTheme();
+
+  const [focused, setFocused] = useState(false);
+
   return (
     <S.Container>
-      <S.Title>Input</S.Title>
+      <S.InputForm
+        placeholder="Adicione uma nova tarefa"
+        placeholderTextColor={theme.colors.base.gray300}
+        focused={focused}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        {...res}
+      />
+
+      <S.ButtonAdd onPress={() => handlePressAdd()}>
+        <Ionicons
+          name="add-circle-outline"
+          size={RFValue(20)}
+          color={theme.colors.base.gray100}
+        />
+      </S.ButtonAdd>
     </S.Container>
   );
 };
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,7 +1,42 @@
+import {RFValue} from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  flex: 1;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  padding: 0 ${RFValue(24)}px;
 `;
 
-export const Title = styled.Text``;
+type InputFormProps = {
+  focused: boolean;
+};
+
+export const InputForm = styled.TextInput<InputFormProps>`
+  flex: 1;
+  height: ${RFValue(54)}px;
+
+  font-family: ${({theme}) => theme.fonts.regular};
+  font-size: ${RFValue(16)}px;
+  line-height: ${RFValue(22.4)}px;
+  color: ${({theme}) => theme.colors.base.gray100};
+
+  border-width: ${RFValue(1)}px;
+  border-color: ${({theme, focused}) =>
+    focused ? theme.colors.product.purpleDark : theme.colors.base.gray700};
+  border-radius: ${RFValue(6)}px;
+  margin-right: ${RFValue(4)}px;
+  padding-left: ${RFValue(16)}px;
+  background-color: ${({theme}) => theme.colors.base.gray500};
+`;
+
+export const ButtonAdd = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  height: ${RFValue(52)}px;
+  width: ${RFValue(52)}px;
+  align-items: center;
+  justify-content: center;
+  border-radius: ${RFValue(6)}px;
+  background-color: ${({theme}) => theme.colors.product.blueDark};
+`;
