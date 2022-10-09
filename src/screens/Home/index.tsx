20990d55b07b48cb5760740ca5d76113be61d5ca
Suéllen Castro <castrosuellenx@gmail.com>
2022-10-08 22:12:37 -0300

    added Input component on Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StatusBar} from 'react-native';
 import {useTheme} from 'styled-components/native';
 
 import Header from '../../components/Header';
+import Input from '../../components/Input';
 import * as S from './styles';
 
 const Home: React.FC = () => {
   const theme = useTheme();
+  const [taskDescription, setTaskDescription] = useState('');
 
   return (
     <S.Container>
@@ -17,6 +19,14 @@ const Home: React.FC = () => {
       />
 
       <Header />
+
+      <S.WrapperInput>
+        <Input
+          value={taskDescription}
+          onChangeText={setTaskDescription}
+          handlePressAdd={() => console.log('Pressed')}
+        />
+      </S.WrapperInput>
     </S.Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,11 @@
+import {RFValue} from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({theme}) => theme.colors.base.gray600};
+`;
+
+export const WrapperInput = styled.View`
+  margin-top: ${RFValue(-27)}px;
 `;
