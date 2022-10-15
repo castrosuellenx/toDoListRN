0a28efa4c8a1747ad6dfc4cd10e6d6fcf59edc0a
Suéllen Castro <castrosuellenx@gmail.com>
2022-10-14 23:42:19 -0300

    added ModalAlert on HomeScreen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {FlatList, StatusBar} from 'react-native';
 import {useTheme} from 'styled-components/native';
 import 'react-native-get-random-values';
@@ -8,6 +8,7 @@ import Header from '../../components/Header';
 import Input from '../../components/Input';
 import Task, {TaskType} from '../../components/Task';
 import Total from '../../components/Total';
+import ModalAlert from '../../components/ModalAlert';
 import * as S from './styles';
 
 const Home: React.FC = () => {
@@ -15,15 +16,27 @@ const Home: React.FC = () => {
 
   const [description, setDescription] = useState('');
   const [tasks, setTasks] = useState<TaskType[]>([] as TaskType[]);
+  const [selectedTask, setSelectedTask] = useState<TaskType | null>(null);
+  const [showModalDelete, setShowModalDelete] = useState(false);
+  const [showModalInvalidTask, setShowModalInvalidTask] = useState(false);
 
-  const handleAddNewTask = (taskDescription: string) => {
+  const handleAddNewTask = useCallback((taskDescription: string) => {
+    if (
+      tasks.find((task) => task.description === taskDescription) ||
+      !taskDescription.replace(/\s/g, '')
+    ) {
+      setShowModalInvalidTask(true);
+      return;
+    }
+
     setTasks((prevState) => [
       ...prevState,
       {id: uuidv4(), description: taskDescription, done: false},
     ]);
-  };
+    setDescription('');
+  }, []);
 
-  const handleToggleDone = (task: TaskType) => {
+  const handleToggleDone = useCallback((task: TaskType) => {
     setTasks((prevState) =>
       prevState.map((item) => {
         if (item === task) {
@@ -35,11 +48,22 @@ const Home: React.FC = () => {
         return item;
       })
     );
-  };
+  }, []);
 
-  const handleDelete = (task: TaskType) => {
+  const handleDelete = useCallback((task: TaskType) => {
+    setSelectedTask(task);
+    setShowModalDelete(true);
+  }, []);
+
+  const onConfirmDelete = useCallback((task: TaskType) => {
     setTasks((prevState) => prevState.filter((item) => item !== task));
-  };
+    setShowModalDelete(false);
+  }, []);
+
+  const onCancelDelete = useCallback(() => {
+    setSelectedTask(null);
+    setShowModalDelete(false);
+  }, []);
 
   return (
     <S.Container>
@@ -103,6 +127,33 @@ const Home: React.FC = () => {
           )}
         />
       </S.Content>
+
+      <ModalAlert
+        visible={showModalDelete}
+        handleConfirm={() => {
+          if (selectedTask) {
+            onConfirmDelete(selectedTask);
+          }
+        }}
+        handleClose={onCancelDelete}
+        data={{
+          title: 'Remove',
+          subtitle: 'Are you sure you want to remove this task?',
+          confirmText: 'Yes',
+          cancelText: 'No',
+        }}
+      />
+
+      <ModalAlert
+        visible={showModalInvalidTask}
+        handleConfirm={() => setShowModalInvalidTask(false)}
+        handleClose={() => setShowModalInvalidTask(false)}
+        data={{
+          title: 'Invalid task',
+          subtitle: 'Task description cannot be empty or duplicate.',
+          confirmText: 'Ok',
+        }}
+      />
     </S.Container>
   );
 };
